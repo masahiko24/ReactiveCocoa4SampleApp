f9e3e2d6df69323f280f31e2d3b0f0863716b0c4
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2016-06-30 01:27:28 +0900

    Improve LoginFormViewController
    - Cleaned up code
    - Moved view model binding into viewModel property's didSet block
    - Added comments

--- ReactiveCocoa4SampleApp/LoginFormViewController.swift
+++ ReactiveCocoa4SampleApp/LoginFormViewController.swift
@@ -12,49 +12,65 @@ import ReactiveCocoa
 
 class LoginFormViewController: UIViewController {
     
+    // MARK: - Login Form User Interfaces
+    
     @IBOutlet weak var usernameField: UITextField!
     
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var loginButton: UIButton!
     
+    // MARK: - Cancel Button
+    
     @IBOutlet weak var cancelButtonItem: UIBarButtonItem!
     
-    var viewModel = LoginFormViewModel()
+    // MARK: - ViewModel
+    
+    var viewModel: LoginFormViewModel! {
+        didSet {
+            // ignore nil
+            guard self.viewModel != nil else {
+                return
+            }
+            
+            // bind viewmodel
+            
+            // bind username and password
+            let usernameSignal = usernameField.rac_textSignal().toSignalProducer()
+                .map { $0 as! String }
+                .flatMapError { _ in SignalProducer<String, NoError>.empty }
+            let passwordSignal = passwordField.rac_textSignal().toSignalProducer()
+                .map { $0 as! String }
+                .flatMapError { _ in SignalProducer<String, NoError>.empty }
+            viewModel.username <~ usernameSignal
+            viewModel.password <~ passwordSignal
+            
+            // bind login action
+            let loginAction = viewModel.loginAction
+            loginButton.rac_command = toRACCommand(Action(enabledIf: loginAction.enabled, { _ in
+                return loginAction.apply()
+                    .map { $0 as AnyObject }
+            }))
+            cancelButtonItem.rac_command = toRACCommand(Action({ (button: AnyObject?) -> SignalProducer<AnyObject, NoError> in
+                self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+                return SignalProducer<AnyObject, NoError>.empty
+            }))
+            
+            // subscribe login results
+            loginAction.values.observeNext { (token) in
+                print("login succeeded. auth token is \(token)")
+            }
+            loginAction.errors.observeNext { (error) in
+                print("login failed.")
+            }
+        }
+    }
+    
+    // MARK: - View Controller
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let usernameSignal = usernameField.rac_textSignal().toSignalProducer()
-            .map { $0 as! String }
-            .flatMapError { _ in SignalProducer<String, NoError>.empty }
-        
-        let passwordSignal = passwordField.rac_textSignal().toSignalProducer()
-            .map { $0 as! String }
-            .flatMapError { _ in SignalProducer<String, NoError>.empty }
-        
-        viewModel.username <~ usernameSignal
-        viewModel.password <~ passwordSignal
-        
-        let action = viewModel.loginAction
-        
-        loginButton.rac_command = toRACCommand(Action(enabledIf: action.enabled, { _ in
-            return action.apply().map { $0 as AnyObject }
-        }))
-        
-        cancelButtonItem.rac_command = toRACCommand(Action({ (button: AnyObject?) -> SignalProducer<AnyObject, NoError> in
-            self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
-            return SignalProducer<AnyObject, NoError>.empty
-        }))
-        
-        action.values.observeNext { (token) in
-            print("login succeeded. auth token is \(token)")
-        }
-        
-        action.errors.observeNext { (error) in
-            print("login failed.")
-        }
-        
+        self.viewModel = LoginFormViewModel()
     }
     
 }
